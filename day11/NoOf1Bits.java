aa6862aee9ee4977b2a4b23bda2f489028a1635d
kkvishvaa <kkvishvaa8787@gmail.com>
2025-08-15 01:46:19 +0530

    day11

---
+++ day11/NoOf1Bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    static int setBits(int n) {
+        // code here
+        int res=0;
+        while(n>0){
+            n=n&(n-1);
+            res+=1;
+        }
+        return res;
+    }
+}
