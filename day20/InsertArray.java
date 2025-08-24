4a5336c79cdd3e01b1f00a8ccec38567be355232
kkvishvaa <kkvishvaa8787@gmail.com>
2025-08-24 23:24:19 +0530

    day20

---
+++ day20/InsertArray.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class InsertArray {
+
+    public void insertAtEnd(ArrayList<Integer> arr, int val) {
+   
+        arr.add(val);
+    }
+}
+    
+
---
+++ day20/InsertAtIndex.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class InsertAtIndex {
+ 
+    public void insertAtIndex(ArrayList<Integer> arr, int index, int val) {
+        // code here
+         arr.add(index,val);
+    }
+}
+
