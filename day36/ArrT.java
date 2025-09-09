00b9cd951a40f63956efc548657b48e973edf8b6
kkvishvaa <kkvishvaa8787@gmail.com>
2025-09-09 23:57:31 +0530

    day36 p5.

--- day36/ArrT.java
+++ day36/ArrT.java
@@ -1,14 +1,29 @@
-public class ArrT {
+//space O(n) approach:
+// public class ArrT {
+//     public void arrange(long[] arr) {
+//         int n=arr.length;
+//         long[] arr2=new long[n];
+//         for(int i=0;i<n;i++){
+//             arr2[i]=arr[(int)arr[i]];
+//         }
+//         for(int i=0;i<n;i++){
+//             arr[i]=arr2[i];
+//         }
+       
+//     }
+// }
+
+
+//time O(n) and space O(1):
+class Solution {
     public void arrange(long[] arr) {
         int n=arr.length;
-        long[] arr2=new long[n];
         for(int i=0;i<n;i++){
-            arr2[i]=arr[(int)arr[i]];
+            arr[i]=arr[i]+(arr[(int)arr[i]]%n)*n;
         }
         for(int i=0;i<n;i++){
-            arr[i]=arr2[i];
+            arr[i]=arr[i]/n;
         }
        
     }
 }
-
