ffbd49eaa0f806fc4e6289de2082818981d13240
kkvishvaa <kkvishvaa8787@gmail.com>
2025-10-03 21:47:50 +0530

    day59

---
+++ day59/SumOfSq.java
@@ -0,0 +1,7 @@
+public class SumOfSq {
+    // Function to calculate the sum of squares of first 'number' natural numbers
+    public int sumOfSquares(int number) {
+        return ((number*(number+1))*(2*number+1))/6;
+    }
+}
+
