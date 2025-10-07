ab1d19aec9991e4aedda13e4bde34728894eacde
kkvishvaa <kkvishvaa8787@gmail.com>
2025-10-07 20:32:58 +0530

    day63

---
+++ day63/NoOfDigits.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class NoOfDigits {
+    //Back-end complete function Template for Java
+
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int digits=(int)Math.log10(n)+1;
+        System.out.println(digits);
+    }
+}
+
